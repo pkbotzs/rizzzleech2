68e6b2e46135ef5c2497f9c1b6f395d3ac379e40
GHLuckM4n <luckm4n@outlook.com>
2024-08-26 21:05:20 +0700

    Force bind port

--- bot/__init__.py
+++ bot/__init__.py
@@ -763,8 +763,7 @@ if ospath.exists('shorteners.txt'):
             if len(temp) == 2:
                 shorteners_list.append({'domain': temp[0],'api_key': temp[1]})
 
-if BASE_URL:
-    Popen(f"gunicorn web.wserver:app --bind 0.0.0.0:{BASE_URL_PORT} --worker-class gevent", shell=True)
+Popen(f"gunicorn web.wserver:app --bind 0.0.0.0:{BASE_URL_PORT} --worker-class gevent", shell=True)
 
 if not ospath.exists('.netrc'):
     with open('.netrc', 'w'):
